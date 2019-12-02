12382e52d8c2c9515ba2ace8013b7901d3c475ec
Dave Clark <Dave.Clark@chorus.co>
2019-12-02 14:03:12 +0000

    seo banner

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,12 +2,29 @@ import React from "react";
 
 import SEO from "../components/seo";
 import Layout from "../layouts/layout";
+import styled from "@emotion/styled";
+import { theme } from "../../config/theme";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  height: calc(100vh - 17.5em - 57px);
+  flex-wrap: wrap;
+  margin-top: 2rem;
+
+  @media (max-width: ${theme.breakpoints.m}) {
+    margin-top: 1.33em;
+  }
+`;
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist.</p>
+    <Container>
+      <SEO title="404: Not found" />
+      <h1>Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist.</p>
+    </Container>
   </Layout>
 );
 
